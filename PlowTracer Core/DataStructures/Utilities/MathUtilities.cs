abd8819d1bf91666ee99cc49c359207da5d6e744
Matt Heimlich <mheimlich@steelcloud.com>
2024-11-19 09:38:39 -0500

    Add multisampling feature to rendering engine
    - Introduced `MultisampleTestKernel` class for enhanced rendering with multiple samples per pixel.
    - Updated `RenderSettings` to include a `Samples` property for configurable sampling rates.
    - Enhanced `ThinLensCamera` to support multisampling via jittered rays for anti-aliasing.
    - Modified `RenderSettingsView`, `MainWindowViewModel`, and related files to incorporate sampling settings in the GUI.
    - Added clamping to pixel color calculations in `CameraTestKernel` to prevent overflow and ensure color accuracy.
    - Expanded `MathUtilities` with a method for generating random points, aiding in multisampling.
    - Refactored code to streamline dependencies and maintain consistency across the project.

--- PlowTracer Core/DataStructures/Utilities/MathUtilities.cs
+++ PlowTracer Core/DataStructures/Utilities/MathUtilities.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace PlowTracer.Core.DataStructures.Utilities;
 
 internal static class MathUtilities
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static float DegreesToRadians(float p_degrees)
     {
         return MathF.PI / 180 * p_degrees;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static float RadiansToDegrees(float p_radians)
     {
         return 180 / MathF.PI * p_radians;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static Vector2 GetRandomPointInUnitSquare()
+    {
+        return new Vector2(Random.Shared.NextSingle() - 0.5f, Random.Shared.NextSingle() - 0.5f);
     }
 }
